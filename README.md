cbc84ded9460f875b41fe4d32a809da79efc0c3f
Zalk0 <88582103+zalk0@users.noreply.github.com>
2025-05-29 21:52:10 +0200

    Config most XL320

---
+++ README.md
@@ -0,0 +1,10 @@
+# Effectors code 2025
+
+Use PlatformIO IDE (CLion or VS Code).
+Synchronize the project.
+
+Servos can do 300° with values from 0 to 1023.
+Useful angles :
+
+- 90° is around 307
+- 180° is around 614
--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,18 @@
 
 #define I2C_ADDRESS          0x20
 #define BAUD_RATE1           115200ul
-#define RX1_PIN              8
-#define TX1_PIN              9
-#define XL320_BANNER_ID      5
+#define RX1_PIN              9
+#define TX1_PIN              8
+#define XL320_BANNER_ID      4
 #define XL320_BANNER_OFFSET  0
 #define XL320_MAGNET_ID      6
-#define XL320_MAGNET_OFFSET  0
+#define XL320_MAGNET_OFFSET  25
 #define XL320_RIGHT_ID       7
-#define XL320_RIGHT_OFFSET   0
+#define XL320_RIGHT_OFFSET   180
 #define XL320_LEFT_ID        8
-#define XL320_LEFT_OFFSET    0
+#define XL320_LEFT_OFFSET    200
 #define XL320_PUMP_ID        9
-#define XL320_PUMP_OFFSET    0
+#define XL320_PUMP_OFFSET    230
 #define DIRECTION_PIN        10u
 #define BAUD_RATE2           57600ul
 #define RX2_PIN              4
@@ -29,48 +29,60 @@
 
 XL320 xl320;
 
-constexpr short SWITCH_PINS[2] = {2, 3};
+// constexpr short SWITCH_PINS[2] = {2, 3};
 
 void setMagnetExtOn() {
     xl320.moveJoint(XL320_RIGHT_ID, XL320_RIGHT_OFFSET);
+    delay(10);
     xl320.moveJoint(XL320_LEFT_ID, XL320_LEFT_OFFSET);
+    delay(10);
 }
 
 void setMagnetExtOff() {
     xl320.moveJoint(XL320_RIGHT_ID, XL320_RIGHT_OFFSET + 614);
+    delay(10);
     xl320.moveJoint(XL320_LEFT_ID, XL320_LEFT_OFFSET + 614);
+    delay(10);
 }
 
 void setBannerOpen() {
     xl320.moveJoint(XL320_BANNER_ID, XL320_BANNER_OFFSET);
+    delay(10);
 }
 
 void setBannerClose() {
-    xl320.moveJoint(XL320_BANNER_ID, XL320_BANNER_OFFSET + 310);
+    xl320.moveJoint(XL320_BANNER_ID, XL320_BANNER_OFFSET + 307);
+    delay(10);
 }
 
 void setMagnetIntOn() {
     xl320.moveJoint(XL320_MAGNET_ID, XL320_MAGNET_OFFSET);
+    delay(10);
 }
 
 void setMagnetIntOff() {
-    xl320.moveJoint(XL320_MAGNET_ID, XL320_MAGNET_OFFSET + 614);
+    xl320.moveJoint(XL320_MAGNET_ID, XL320_MAGNET_OFFSET + 200);
+    delay(10);
 }
 
 void setPumpUp() {
     xl320.moveJoint(XL320_PUMP_ID, XL320_PUMP_OFFSET);
+    delay(10);
 }
 
 void setPumpDown() {
-    xl320.moveJoint(XL320_PUMP_ID, XL320_PUMP_OFFSET + 310);
+    xl320.moveJoint(XL320_PUMP_ID, XL320_PUMP_OFFSET + 307);
+    delay(10);
 }
 
 void setPliersUp() {
     ax12a.move(AX12A_ID, AX12A_OFFSET);
+    delay(10);
 }
 
 void setPliersDown() {
     ax12a.move(AX12A_ID, AX12A_OFFSET + 400);
+    delay(10);
 }
 
 void setPumpOn() {
@@ -116,13 +128,14 @@ void putDownCans() {
 }
 
 void takeEverything() {
-    setPliersDown();
-    takePlank();
+    // setPliersDown();
     takeCans();
+    delay(200);
+    takePlank();
 }
 
 void putDownEverything() {
-    setPliersDown();
+    // setPliersDown();
     putDownCans();
     putDownPlank();
 }
@@ -169,6 +182,7 @@ void setup() {
     Serial.begin(BAUD_RATE1);
 
     // Init Wire I2C
+    /*
     Wire.begin(I2C_ADDRESS);
     Serial.println("I2C Init");
     Wire.onReceive(receiveEvent);
@@ -177,6 +191,7 @@ void setup() {
     for (const unsigned char i : SWITCH_PINS) {
         pinMode(i, INPUT);
     }
+    */
 
     // Init Servo XL320 et AX-12A
     Serial1.begin(BAUD_RATE1, SERIAL_8N1, RX1_PIN, TX1_PIN);
@@ -184,12 +199,13 @@ void setup() {
     for (const int id : {
              XL320_BANNER_ID, XL320_MAGNET_ID, XL320_RIGHT_ID, XL320_LEFT_ID, XL320_PUMP_ID
          }) {
-        xl320.setJointSpeed(id, 500);
+        xl320.setJointSpeed(id, 800);
+        delay(10);
     }
     Serial2.begin(BAUD_RATE2, SERIAL_8N1, RX2_PIN, TX2_PIN);
     ax12a.begin(BAUD_RATE2, DIRECTION_PIN, &Serial2);
     ax12a.setEndless(AX12A_ID, OFF);
-    setBannerOpen();
+    setPumpUp();
 }
 
 void loop() {
